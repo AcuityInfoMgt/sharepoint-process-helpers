021b4dd65ec89c3203587bcf0f9f9299b1520a14
Corey Martin <coreym@gmail.com>
2014-09-25 11:17:42 -0400

    Create guidance.js

---
+++ js/guidance.js
@@ -0,0 +1,24 @@
+sharepointProcessHelpers.guidance = sharepointProcessHelpers.guidance || {};
+
+sharepointProcessHelpers.guidance.config = { 
+	guidanceField: 'Guidance', /* field in the Status list that contains the guidance for this status */
+};
+
+sharepointProcessHelpers.guidance.getGuidance = function () {
+	sharepointProcessHelpers.core.getListItemById(sharepointProcessHelpers.core.config.packagesList, sharepointProcessHelpers.core.queryObj()['ID'], sharepointProcessHelpers.core.config.statusField).done(
+		function(ListItem) {
+			if (ListItem.get_item(sharepointProcessHelpers.core.config.statusField)) {
+				sharepointProcessHelpers.core.getListItemById(sharepointProcessHelpers.core.config.statusesList, ListItem.get_item(sharepointProcessHelpers.core.config.statusField).get_lookupId(), sharepointProcessHelpers.guidance.config.guidanceField).done(
+					function(ListItem) {
+						/*if(sharepointProcessHelpers.guidance.config.guidanceField.length > 0) {
+							$('#sharepoint-process-helpers_guidance').show();
+						}*/
+						$('#sharepoint-process-helpers_guidance_content').html(ListItem.get_item(sharepointProcessHelpers.guidance.config.guidanceField));
+					}
+				);
+			}
+		}
+	);
+}
+
+_spBodyOnLoadFunctionNames.push("sharepointProcessHelpers.guidance.getGuidance");
